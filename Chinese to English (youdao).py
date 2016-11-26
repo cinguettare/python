b6ae432f41695dc04e81bd1045a3ba3b5a8c3e2d
cinguettare <13265656727@163.com>
2016-11-27 00:45:50 +0800

    Create Chinese to English (youdao).py

---
+++ Chinese to English (youdao).py
@@ -0,0 +1,23 @@
+# -*- coding: <utf-8> -*-
+
+import json              # Load json
+import urllib.parse      # Load urllib.parse module
+import urllib.request    # Load urllib.request module
+
+
+Chinese = input("Please enter what you need to translate: ")
+
+url = 'http://fanyi.youdao.com/...'                            # Youdao translation page copy url
+
+data = {'type': 'AUTO', 'i': content, 'doctype': 'json', 'xmlVersion': '1.8', 
+        'keyfrom': 'fanyi.web', 'ue': 'UTF-8', 'action': 'FY_BY_CLICKBUTTON', 'typoResult': 'true'}
+        # analog data
+
+data = urllib.parse.urlencode(data).encode('utf-8')            # Analysis, Change the encoding format
+
+response = urllib.request.urlopen(url, data)                   
+html = response.read().decode('utf-8')                         # Conversion encoding
+
+target = json.loads(html)                                      # Use json to convert a str to a dict
+
+print("English：%s" %target['translateResult'][0][0]['tgt'])   
