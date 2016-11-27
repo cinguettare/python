968f938374626698de1fea272a37e7cd16936cab
cinguettare <13265656727@163.com>
2016-11-27 14:37:11 +0800

    Create number game.py

---
+++ number game.py
@@ -0,0 +1,49 @@
+import easygui as g
+import sys
+import random
+
+
+num = random.randint(1, 10)
+
+def choice():
+    msg = "Do you want to start the game again?"
+    title = "please choose"
+    
+    if g.ccbox(msg, title):
+        pass
+    else:
+        sys.exit(0)
+        
+
+
+def game():
+    times = 3
+    
+    while 1:
+
+        times = times - 1
+    
+        msg = 'Try to guess what the number is (1~10): '
+        title = 'number game'
+        E_num = g.integerbox(msg, title, lowerbound=1, upperbound=10)
+
+        if E_num == num:
+            g.msgbox("Yeah, you're right!")
+            choice()
+        else:
+            if E_num > num:
+                g.msgbox("Oh, it's big!")
+
+            else:
+                g.msgbox("No, it's small!")
+
+            if times > 0:
+                g.msgbox("Come again!")
+                
+            else:
+                g.msgbox("Opportunity to run out!")
+                choice()
+
+
+if __name__ == "__main__":
+    game()
